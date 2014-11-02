6ce68ce4097fee2f5d51e6bc0b5bbdec13bb2cd4
孔庆威 <4878802@qq.com>
2014-11-02 23:21:03 +0800

    2123

--- 口袋理财/src/com/itheima/koudai/MainActivity.java
+++ 口袋理财/src/com/itheima/koudai/MainActivity.java
@@ -2,7 +2,6 @@ package com.itheima.koudai;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 
@@ -12,12 +11,4 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
---
+++ 口袋理财/src/com/itheima/koudai/Two.java
@@ -0,0 +1,5 @@
+package com.itheima.koudai;
+
+public class Two {
+
+}
